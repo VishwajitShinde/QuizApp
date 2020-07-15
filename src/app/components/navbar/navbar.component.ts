e25182d9d89f9f817d7df50d79126ce5dcf4a641
dhamma4u <dhamma4u@gmail.com>
2020-07-15 23:56:15 +0530

    Added Menubar : Just Copied From Old - Need to Update

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {TranslateService} from '@ngx-translate/core';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  public translate: TranslateService;
+
+  constructor(translate: TranslateService) {
+    this.translate = translate;
+
+    translate.addLangs(['en', 'mar', 'hin']);
+
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('en');
+
+     // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translate.use('en');
+}
 
   ngOnInit(): void {
   }
